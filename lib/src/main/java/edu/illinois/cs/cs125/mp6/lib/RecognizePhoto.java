35358751d24f5b58fb24757246adc3b94e9fe7ee
Geoffrey Challen <challen@illinois.edu>
2018-04-03 18:30:00 -0500

    Initial commit.

---
+++ lib/src/main/java/edu/illinois/cs/cs125/mp6/lib/RecognizePhoto.java
@@ -0,0 +1,34 @@
+package edu.illinois.cs.cs125.mp6.lib;
+
+public final class RecognizePhoto {
+
+    /**
+     * Get the image width.
+     *
+     * @param json the JSON string returned by the Microsoft Cognitive Services API
+     * @return the width of the image or 0 on failure
+     */
+    public static int getWidth(final String json) {
+        return 0;
+    }
+
+    /**
+     * Get the image height.
+     *
+     * @param json the JSON string returned by the Microsoft Cognitive Services API
+     * @return the width of the image or 0 on failure
+     */
+    public static int getHeight(final String json) {
+        return 0;
+    }
+
+    /**
+     * Get the image file type.
+     *
+     * @param json the JSON string returned by the Microsoft Cognitive Services API
+     * @return the type of the image or null
+     */
+    public static String getFormat(final String json) {
+        return "";
+    }
+}
